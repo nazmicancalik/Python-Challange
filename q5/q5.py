be8921d29b878138836ce0b9cc0b033a966f898a
nazmicancalik <nazmicancalik@gmail.com>
2017-12-08 12:19:23 +0300

    Got the string in a concatenated manner.

--- q5/q5.py
+++ q5/q5.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 
+import sys
+
 def main(argv):
-	
+	file = sys.argv[1]
+	data = []
+	with open(file) as fileobj:
+		for line in fileobj:
+			data.append(line.strip())
+	print (''.join(data))
 
 if __name__ == '__main__':
 	main(sys.argv)
